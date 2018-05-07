b59f1bbbd79a7c0adefd382effd59ceee34e7796
Ryan Siu <ryansiu1995@gmail.com>
2018-05-07 23:08:53 +0800

    Create the support for notification push in service worker

--- src/sw-src.js
+++ src/sw-src.js
@@ -3,3 +3,12 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox
 workbox.precaching.precacheAndRoute([]);
 
 workbox.strategies.networkFirst();
+
+// Adding the listener to the push event
+self.addEventListener('push', function(e) {
+    // TODO Handle the notification push
+    console.log(e.data.text());
+    e.waitUntil(
+        self.registration.showNotification('Hello world!')
+    );
+});
