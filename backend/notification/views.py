9372ab00fc882601540a8bec1b770af172d6db51
Ryan Siu <ryansiu@uw.edu>
2018-05-09 08:40:28 -0700

    API reformat

--- backend/notification/views.py
+++ backend/notification/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from notification.models import *
 from notification.serializers import *
 
+from rest_framework import viewsets
+
 # Create your views here.
 class NotificationSubscriptionView(viewsets.ModelViewSet):
     queryset = NotificationSubscription.objects.all()
