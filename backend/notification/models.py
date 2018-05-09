40f6b57de4fd7d0f875e7d6af411232dd51734da
Ryan Siu <ryansiu@uw.edu>
2018-05-09 07:05:38 -0700

    Notification token database initialized

--- backend/notification/models.py
+++ backend/notification/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class NotificationSubscription(models.Model):
+    endpoint = models.TextField()
+    p256dh = models.TextField()
+    auth = models.TextField()
+    last_upload = models.DateField(auto_now_add = True)
+    
+    class Meta:
+        db_table = "notification_token"
--- backend/notification/views.py
+++ backend/notification/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from notification.models import *
+from notification.serializers import *
 
 # Create your views here.
+class NotificationSubscriptionView(viewsets.ModelViewSet):
+    queryset = NotificationSubscription.objects.all()
+    serializer_class = NotificationSubscriptionSerializer
