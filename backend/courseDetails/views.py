83d8fbbb68ba1a17aed6311179af5673a78f2f15
Ryan Siu <ryansiu@uw.edu>
2018-05-11 08:13:47 -0700

    Override the POST method in notification table

--- backend/courseDetails/views.py
+++ backend/courseDetails/views.py
@@ -1,7 +1,9 @@
 from courseDetails.models import *
 from courseDetails.serializers import *
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
 
 # Create your views here.
 class AssignmentViewSet(viewsets.ModelViewSet):
@@ -19,6 +21,19 @@ class LTNoteViewSet(viewsets.ModelViewSet):
 class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            cursor = Notification.objects.create(**serializer.validated_data)
+            cursor.save()
+            return Response(
+                serializer.validated_data, status=status.HTTP_201_CREATED
+            )
+        return Response({
+            'status': 'Bad request',
+            'message': 'Account could not be created with received data.'
+        }, status=status.HTTP_400_BAD_REQUEST)
 
 class PeopleViewSet(viewsets.ModelViewSet):
     queryset = People.objects.all()
