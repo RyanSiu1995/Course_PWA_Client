ac4566bdb5ae07faad255ba68721eeb6e42c0353
Ryan Siu <ryansiu@uw.edu>
2018-05-12 09:10:10 -0700

    Credential added with other bug fixations

--- private/routes.js
+++ private/routes.js
@@ -1,74 +1,80 @@
 const axios = require('axios');
 
 module.exports = (app, serverAddress) => {
+  const header = {
+    auth: {
+      username: 'frontend',
+      password: 'frontend'
+    }
+  }
   app.get('/api/info', function (req, res) {
 
-    axios.get(serverAddress + '/courseInfo/?format=json')
-      .then(function (response) {
-        console.log(response.data);
-        res.json(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.send(error.toString());
-      });
+    axios.get(serverAddress + '/courseInfo/?format=json', header)
+    .then(function (response) {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.send(error.toString());
+    });
 
   });
 
   app.get('/api/lectures', function (req, res) {
 
-    axios.get(serverAddress + '/lt_note/?format=json')
-      .then(function (response) {
-        //console.log(response.data);
-        res.json(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.send(error.toString());
-      });
+    axios.get(serverAddress + '/lt_note/?format=json', header)
+    .then(function (response) {
+      //console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.send(error.toString());
+    });
 
   });
 
   app.get('/api/assignments', function (req, res) {
 
-    axios.get(serverAddress + '/assignment/?format=json')
-      .then(function (response) {
-        //console.log(response.data);
-        res.json(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.send(error.toString());
-      });
+    axios.get(serverAddress + '/assignment/?format=json', header)
+    .then(function (response) {
+      //console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.send(error.toString());
+    });
 
   });
 
   app.get('/api/tutorials', function (req, res) {
 
-    axios.get(serverAddress + '/tutorial_note/?format=json')
-      .then(function (response) {
-        console.log(response.data);
-        res.json(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.send(error.toString());
-      });
+    axios.get(serverAddress + '/tutorial_note/?format=json', header)
+    .then(function (response) {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.send(error.toString());
+    });
 
   });
 
 
   app.get('/api/staff', function (req, res) {
 
-    axios.get(serverAddress + '/teaching_staff/?format=json')
-      .then(function (response) {
-        console.log(response.data);
-        res.json(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-        res.send(error.toString());
-      });
+    axios.get(serverAddress + '/teaching_staff/?format=json', header)
+    .then(function (response) {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.send(error.toString());
+    });
 
   });
 
--- private/server.js
+++ private/server.js
@@ -6,7 +6,7 @@ const port = 8080;
 
 const backendAPIAddress = (function(){
     if (process.env.NODE_ENV === 'production') {
-        return 'http://0.0.0.0:80/course';
+        return 'http://backend:80/course';
     } else {
         return 'http://127.0.0.1:8000/course';
     }
@@ -17,7 +17,6 @@ console.log('backendAddress:', backendAPIAddress)
 var app = express();
 // WebSocket for notification push
 var WebSocketClient = require('websocket').client;
-var client = new WebSocketClient();
 var socketConnection;
 // Web Push
 var serverKey = 'AAAAv0b6qUo:APA91bHH6ogyI5jVz7VOuYq6_oKpJOxBts9tUnnrOj_NBh2uy-Ea0BF6ZkcG4ei37TRm3fmjw_vsmDzMUK066SfZTDnThtXgdA-NBV-7Al21EQmE-qwrivzKuYdpymfoJcVNZkqs3A96';
@@ -65,60 +64,53 @@ app.listen(port, function () {
     console.log('Server is now running at ' + port);
 });
 
-client.on('connectFailed', function (error) {
-    console.log('Connect Error: ' + error.toString());
-});
-
-client.on('connect', function (connection) {
-    console.log('Backend WebSocket Client Connected');
-    // Globalize the connection
-    socketConnection = connection;
-
-    connection.on('error', function (error) {
-        console.log("Connection Error: " + error.toString());
+const clientconnection = function() {
+    let client = new WebSocketClient();
+    client.on('connectFailed', function (error) {
+        console.log('Connect Error: ' + error.toString());
+        console.log('Trying to reconnect...')
+        setTimeout(clientconnection, 1000);
     });
-
-    connection.on('close', function () {
-        console.log('Connection Closed');
+    
+    client.on('connect', function (connection) {
+        console.log('Backend WebSocket Client Connected');
+        // Globalize the connection
+        socketConnection = connection;
+    
+        connection.on('error', function (error) {
+            console.log("Connection Error: " + error.toString());
+        });
+    
+        connection.on('close', function () {
+            console.log('Connection Closed');
+        });
+    
+        connection.on('message', function (message) {
+            // TODO receive the server push
+            if (message.type === 'utf8') {
+                console.log(message);
+                // const body = JSON.parse(message.utf8Data);
+                // const pushSubscription = {
+                //     endpoint: body.endpoint,
+                //     keys: {
+                //         p256dh: body.p256dh,
+                //         auth: body.auth
+                //     }
+                // };
+                // webpush.sendNotification(
+                //     pushSubscription,
+                //     body.message
+                // ).then(res => {
+                //     console.log(res);
+                // })
+                // .catch(res => {
+                //     console.log(res);
+                // });
+            }
+        });
     });
+    // Start the connection to websocket
+    client.connect('ws://backend:80/ws/');
+}
 
-    connection.on('message', function (message) {
-        // TODO receive the server push
-        if (message.type === 'utf8') {
-            console.log(message);
-            const body = JSON.parse(message.utf8Data);
-            const pushSubscription = {
-                endpoint: body.endpoint,
-                keys: {
-                    p256dh: body.p256dh,
-                    auth: body.auth
-                }
-            };
-            webpush.sendNotification(
-                pushSubscription,
-                body.message
-            ).then(res => {
-                console.log(res);
-            })
-            .catch(res => {
-                console.log(res);
-            });
-        }
-    });
-
-    function sendToServer() {
-        if (connection.connected) {
-            const pushSubscription = {
-                endpoint: 'https://android.googleapis.com/gcm/send/fjeF6OZWG2Y:APA91bFXFQT-PrVYU1RCZwPSvq3vLFnTg78PH3Khu92fyOa8CwFJVj-rSBruEuY8vC8i1dQOTPLDGL3ealL5HxID0C1Tt-r7QLWowiWXLxPqF0ZxleEj4uEgzXYluVdOtpgZNuD-ai6G',
-                keys: {
-                    p256dh: "BArxCil4sJ+gJECjRGUyfqA1AnNKRHfzTdOxt5TIP9f8B6YaezXYtLQ8EK1T0WFsY5OemlKHbMOP4/5v4U8MKJE=",
-                    auth: "1gDbmdI151x3KTV+5LxZig=="
-                }
-            };
-            connection.sendUTF(JSON.stringify(pushSubscription));
-        }
-    }
-    // sendToServer();
-});
-// Start the connection to websocket
-client.connect('ws://backend:80/ws/');
+clientconnection();
