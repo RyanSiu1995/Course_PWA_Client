649e82544feb9b66e40925d05297000fd18f542c
Ryan Siu <ryansiu@uw.edu>
2018-05-12 23:13:15 -0700

    Production environment fixation

--- backend/courseBackend/settings.py
+++ backend/courseBackend/settings.py
@@ -134,14 +134,22 @@ REST_FRAMEWORK = {
 
 ASGI_APPLICATION = "notification.routing.application"
 
+hosts = []
+try:
+    os.environ['ENV']
+    hosts = [
+        ("redis", 6379)
+    ]
+except:
+    hosts = [
+        ("127.0.0.1", 6379)
+    ]
+
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [
-                ("127.0.0.1", 6379),
-                ("redis", 6379)
-            ],
+            "hosts": hosts,
         },
     },
 }
